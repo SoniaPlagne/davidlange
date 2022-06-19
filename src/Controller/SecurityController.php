03f080a329d042b72a3dc974c17bf3cba2e5499f
Plagne <splagne@gmail.com>
2022-06-20 01:31:30 +0200

    MAJ SecurityController log_collaborateur

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -33,4 +33,37 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+
+    /**
+     * @Route("/log_collaborateur", name="log_collaborateur")
+     */
+    public function log_collaborateur(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('@EasyAdmin/page/login.html.twig', [
+            // parameters usually defined in Symfony login forms
+            'error' => $error,
+            'last_username' => $lastUsername,
+            'page_title' => 'Connexion au Tableau de Bord David Lange',
+            'csrf_token_intention' => 'authenticate',
+            'target_path' => $this->generateUrl('admin'),
+            'username_label' => 'Votre email',
+            'password_label' => 'Votre mot de passe',
+            'sign_in_label' => 'Connexion',
+            'username_parameter' => 'my_custom_username_field',
+            'password_parameter' => 'my_custom_password_field',
+            #'forgot_password_path' => $this->generateUrl('...', ['...' => '...']),
+            #'forgot_password_label' => 'Mot de passe oublié?',
+            'remember_me_enabled' => true,
+            'remember_me_parameter' => 'custom_remember_me_param',
+            'remember_me_checked' => true,
+            'remember_me_label' => 'Remember me',
+        ]);
+
+    }
 }
+
+
